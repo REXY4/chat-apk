284e556d697f95c748f8ba8e099dd7cc49fbdd0f
REXY4 <muhammadrzki96@gmail.com>
2022-03-14 04:23:51 +0700

    add

---
+++ seeders/20210724133318-user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          email: 'admin@mail.com',
+          password:
+            '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
+          name: 'admin',
+          status: 'admin',
+        },
+        {
+          email: 'user1@mail.com',
+          password:
+            '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
+          name: 'user 01',
+          status: 'customer',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
